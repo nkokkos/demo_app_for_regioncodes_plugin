e2cec7e306716539a587fe0c31997018de3bc601
nkokkos@gmail.com <nkokkos@gmail.com>
2010-02-04 17:15:42 +0200

    reimplemented class methods, added geographical region, added doc

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,33 +1,78 @@
 class TestController < ApplicationController
 
   def index
-
-    @nomos  =  Regioncode.nomos("") # need to pass an empty string to return all regions
-    @dhmotiko_diamerisma = []
-    @dhmos = []
-
+    @region             =   []
+    @region             =   Regioncode.geographical_region() # call it without parameters to get all regions
+    @department      =   []
+    @municipality      =   []
+    @admin_district   =   []
+    @commune         =   []
+    
   end
 
-  def update_dhmotiko_diamerisma
+  def update_department
     
-    @nomos  =  Regioncode.nomos(params[:code])
-    @dhmotiko_diamerisma = Regioncode.dhmotiko_diamerisma(params[:code])
-    @dhmos = []
-
+     @region =  Regioncode.geographical_region(params[:region]) 
+     @department = Regioncode.department(params[:region])   # you get department from region
+     @municipality = Regioncode.municipality(params[:region]) 
+     @admin_district = []
+     @commune = []
+    
     render :update do |page|
-      page.replace_html 'dhmotiko_diamerisma', :partial => 'dhmotiko_diamerisma', :object => @dhmotiko_diamerisma
-      page.replace_html 'dhmos', :partial => 'dhmos', :object => @dhmos
+      page.replace_html 'department', :partial => 'department', :object => @department
+      page.replace_html 'municipality', :partial => 'municipality', :object => @municipality
+      page.replace_html 'admin_district', :partial => 'admin_district', :object => @admin_district
+      page.replace_html 'commune', :partial => 'commune', :object => @commune
     end
   
   end
 
-  def update_dhmos
-
-   @dhmos = Regioncode.dhmos_or_oikismos(params[:code])
-
+   def update_municipality
+    
+   #  @region =  Regioncode.nomos(params[:region])
+   #  @department = Regioncode.department(params[:region])
+     @municipality = Regioncode.municipality(params[:department]) # you get municipality from department
+     @admin_district = []
+     @commune =       []
+     
     render :update do |page|
-      page.replace_html 'dhmos', :partial => 'dhmos', :object => @dhmos
+    #  page.replace_html 'department', :partial => 'department', :object => @department
+      page.replace_html 'municipality', :partial => 'municipality', :object => @municipality
+      page.replace_html 'admin_district', :partial => 'admin_district', :object => @admin_district
+      page.replace_html 'commune', :partial => 'commune', :object => @commune
     end
   end
 
+  def update_admin_district
+    
+   #  @region =  Regioncode.nomos(params[:region])
+   #  @department = Regioncode.department(params[:region])
+   #  @municipality = Regioncode.department(params[:region])
+     @admin_district = Regioncode.administrative_district(params[:municipality]) # you get admin_district from municipality
+     @commune = []
+     
+    render :update do |page|
+    #  page.replace_html 'department', :partial => 'department', :object => @department
+    #  page.replace_html 'municipality', :partial => 'municipality', :object => @municipality
+      page.replace_html 'admin_district', :partial => 'admin_district', :object => @admin_district
+      page.replace_html 'commune', :partial => 'commune', :object => @commune
+    end
+  end
+  
+   def update_commune
+    
+   #  @region =  Regioncode.nomos(params[:region])
+   #  @department = Regioncode.department(params[:region])
+   #  @municipality = Regioncode.department(params[:region])
+   #  @admin_district = Regioncode.department(params[:admin_district])
+     @commune = Regioncode.commune(params[:admin_district] )  # you get commune from admin_district
+     
+    render :update do |page|
+    #  page.replace_html 'department', :partial => 'department', :object => @department
+    #  page.replace_html 'municipality', :partial => 'municipality', :object => @municipality
+    #  page.replace_html 'admin_district', :partial => 'admin_district', :object => @admin_district
+      page.replace_html 'commune', :partial => 'commune', :object => @commune
+    end
+  end
+  
 end
--- vendor/plugins/regioncodes/lib/app/models/regioncode.rb
+++ vendor/plugins/regioncodes/lib/app/models/regioncode.rb
@@ -1,31 +1,104 @@
 class Regioncode < ActiveRecord::Base
-   
-    def self.nomos(str)
+    # Regioncode table has three fields: code_index, code and description
+    # code_index has values from 1 to 6 
+    # code has a unique value for every entry in the table
+    # description contains the name of region; that being municipality, department or commune
+    
+    # Official translation of terms:
+    # Region: Γεωγραφική Περιοχή
+    # Department : Νομός
+    # Municipality: Δήμος
+    # Administrative District : Τ.Δ
+    # Commune : Κοινότητα    
 
+    # Here's how I implemented the retrieval:
+    
+    # code has always a range of 8 ascii characters.  Let's use the entry:
+    # code_index, code, description
+    # 6;01010101;Μεσολόγγιον,το;
+    # as an example.
+    
+    # the very first character "0" represents the region of Greece where this entry resides; therefore
+    # for "0" the region is "ΛΟΙΠΗ ΣΤΕΡΕΑ ΕΛΛΑΣ ΚΑΙ ΕΥΒΟΙΑ"
+    
+    # the two first characters combined represent the department (Nomos) where this entry resides
+    # for "01" the department is "ΝΟΜΟΣ ΑΙΤΩΛΙΑΣ ΚΑΙ ΑΚΑΡΝΑΝΙΑΣ"
+    
+    # the 4 first characters combined represent the municipality where this entry resides
+    # for "0101" the municipality is "ΔΗΜΟΣ ΙΕΡΑΣ ΠΟΛΗΣ ΜΕΣΟΛΟΓΓΙΟΥ"
+    
+    # the 6 first characters combined represent the administrative district where this entry resides
+    # for 010101 the district is "Τ.Δ.Μεσολογγίου"
+    
+    # and finally the whole 8 characters represent the commune where this entry resides
+    # for 01010101 the commune is Μεσολόγγιον
+    
+    # So the level we use is ΛΟΙΠΗ ΣΤΕΡΕΑ ΕΛΛΑΣ ΚΑΙ ΕΥΒΟΙΑ-> ΝΟΜΟΣ ΑΙΤΩΛΙΑΣ ΚΑΙ ΑΚΑΡΝΑΝΙΑΣ
+    # -> ΔΗΜΟΣ ΙΕΡΑΣ ΠΟΛΗΣ ΜΕΣΟΛΟΓΓΙΟΥ->Τ.Δ.Μεσολογγίου->Μεσολόγγιον
+    
+    # the above combined with the code_index level lets you retrieve the entries quickly, so :
+    # for code_index = 1, you get back the geographical region (Γεωγραφική Περιοχή)
+    # for code_index = 2, you get back the department (Νομός)
+    # for code_index = 4, you get back the municipality (Δήμος)
+    # for code_index = 5, you get back the administrative department (Διοικητικό Διαμέρισμα)
+    # for code_index = 6, you get back the commune (Οικισμός)
+    
+    def self.geographical_region(str="")
+       # class method returns all the geographical regions if you call it without parameters
+       # e.g myRegions = Regioncode.geographical_region(), will return all the regions (that should  be ten)
+       # if you call it with parameters, the parameter you pass is the code 
+       # e,g, myRegions = Regioncode.geographical_region("01010101"), will give you -> "0" -> ΛΟΙΠΗ ΣΤΕΡΕΑ ΕΛΛΑΣ ΚΑΙ ΕΥΒΟΙΑ
+         if str.empty? 
+            Regioncode.find_by_sql('select description, code from regioncodes where code_index = 1')
+          else
+            search = str.to_s + "%"
+            Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 1 and code like ? ", search])
+         end
+          
+    end
+   
+    def self.department(str="")
       if str.empty? 
       	Regioncode.find_by_sql('select description, code from regioncodes where code_index = 2')
       else
         str = str.to_s
-        search = str[0..1]
+        search = str[0..1]     # grab first 2 characters
         search = search + "%"
         Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 2 and code like ? ", search])
       end
-
     end
     
-    def self.dhmotiko_diamerisma(str)
-      str = str.to_s
-      search = str[0..1]
-      search = search + "%"
-      Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 5 and code like ? ", search])
+    def self.municipality(str="")
+      if str.empty?
+        Regioncode.find_by_sql("select description, code from regioncodes where code_index = 4")
+      else
+       str = str.to_s
+       search = str[0..1]      # grap 2 first characters but different code_level (=4)
+       search = search + "%"
+       Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 4 and code like ? ", search])
+      end
+    end 
+
+    def self.administrative_district(str="")
+      if str.empty? 
+        Regioncode.find_by_sql("select description, code from regioncodes where code_index = 5")
+      else
+       str = str.to_s
+       search = str[0..4]    # grap first 4 characters
+       search = search + "%"
+       Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 5 and code like ? ", search])
+      end
     end 
     
-    def self.dhmos_or_oikismos(str)
-      str = str.to_s
-      search = str[0..5]
-      search = search + "%"
-      Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 6 and code like ? ", search])
+    def self.commune(str="")
+       if str.empty? 
+        Regioncode.find_by_sql("select description, code from regioncodes where code_index = 6")
+       else
+        str = str.to_s
+        search = str[0..5]    # grab first 5 characters but use different code_level (=6)
+        search = search + "%"
+        Regioncode.find_by_sql(["select description, code from regioncodes where code_index = 6 and code like ? ", search])
+      end
     end
     
-end
-    
+end 
